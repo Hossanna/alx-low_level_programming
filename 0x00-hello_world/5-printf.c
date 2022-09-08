4c2c1dbf7315527a949a4565da7e42e40c6baa79
Hosanna Bivan <70913083+Hossanna@users.noreply.github.com>
2022-09-08 23:10:28 +0100

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
- * main - prints the phrase with proper grammer
+ * main - prints the phrase with proper grammar,
  * but the outcome is a piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
